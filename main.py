9b241787dd8f77463fd41244dfb85370765b2d04
ddigimon <965790989@qq.com>
2019-02-15 22:08:28 +0800

    add main file

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ from train_method import train_method
 from DataSet import dataset
 from model import RC_model
 
+def para_arg():
+    paraser=argparse.ArgumentParser('NLPDemo')
+    paraser.add_argument('--mode',type=str,default='train',help='mode choose')
+
+    return paraser
 
 if __name__ == '__main__':
-    mode='train'
     with open('config.yaml',encoding='utf-8') as config_file:
         config=yaml.load(config_file)
 
+    arg=para_arg().parse_args()
 
     data=dataset(config)
     data.load_data()
     data.load_init()
 
-    if mode=='train':
+    if arg.mode=='train':
         model=RC_model(config,mode='train')
-        model.build_model()
+    else:
+        model=RC_model(config,mode='test')
+
+    model.build_model()
 
     train=train_method(data=data,model=model,config=config)
     train.train()
